cbf6ff56fafe20fac1a994c33d9705ab3e782855
anoud75 <anoudsaud75@gmail.com>
2025-08-26 14:54:33 +0000

    variables_if_else_while: 4-print_alphabt (skip e and q)

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,19 @@
-<<<<<<< HEAD
-
-=======
 #include <stdio.h>
+
 /**
  * main - prints the alphabet in lowercase except e and q
  * Return: 0
  */
 int main(void)
 {
-    char ch;
+        char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-        if (ch != 'e' && ch != 'q')
-            putchar(ch);
-
-    putchar('\n');
-    return (0);
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                if (ch == 'e' || ch == 'q')
+                        continue;
+                putchar(ch);
+        }
+        putchar('\n');
+        return (0);
 }
->>>>>>> c4b3d23 (variables_if_else_while: 4-print_alphabt (skip e and q))
