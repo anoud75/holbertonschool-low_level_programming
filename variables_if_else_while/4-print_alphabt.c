5e579ae18dc46559bc8a5c1a66a47748457f83d6
anoud75 <anoudsaud75@gmail.com>
2025-08-26 15:05:14 +0000

    Fix Betty: use tabs for indentation in 4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-        char ch;
+	char ch;
 
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-                if (ch == 'e' || ch == 'q')
-                        continue;
-                putchar(ch);
-        }
-        putchar('\n');
-        return (0);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+			continue;
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
 }
