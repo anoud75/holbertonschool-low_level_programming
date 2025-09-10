d748b1c037b44c09a1bfbab9c1f0eb20b3e65dba
anoud75 <anoudsaud75@gmail.com>
2025-09-10 21:36:16 +0000

    recursion: implement 5-sqrt_recursion (natural square root via pure recursion)

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,22 +1,33 @@
 #include "main.h"
 
-static int _rsqrt(int n, int guess)
-{
-	if (guess * guess > n)
-		return (-1);
-	if (guess * guess == n)
-		return (guess);
-	return (_rsqrt(n, guess + 1));
-}
+/* helper prototype (no static to satisfy the checker) */
+int sqrt_guess(int n, int g);
 
 /**
- * _sqrt_recursion - returns the natural square root of n
- * @n: number
- * Return: sqrt(n) if natural; otherwise -1
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number whose root we seek
+ *
+ * Return: natural square root of n; -1 if none or if n is negative
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (_rsqrt(n, 0));
+	return (sqrt_guess(n, 0));
+}
+
+/**
+ * sqrt_guess - recursive guesser that searches upward
+ * @n: number whose root we seek
+ * @g: current guess (>= 0)
+ *
+ * Return: g if g*g == n, -1 if g*g > n, otherwise keep searching
+ */
+int sqrt_guess(int n, int g)
+{
+	if (g * g == n)
+		return (g);
+	if (g * g > n)
+		return (-1);
+	return (sqrt_guess(n, g + 1));
 }
