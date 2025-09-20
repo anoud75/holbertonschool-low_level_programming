1670c79ccbf4fa9db294f8039218d8b85d6014c4
anoud75 <anoudsaud75@gmail.com>
2025-09-20 19:32:06 +0000

    malloc_free: implement 4-free_grid (free 2D grid)

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees a 2D grid created by alloc_grid
+ * @grid: pointer to the grid
+ * @height: number of rows
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+		return;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
