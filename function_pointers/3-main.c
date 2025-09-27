6621d40aec81b79baf882c8299e312c6a17dff1a
anoud75 <anoudsaud75@gmail.com>
2025-09-27 12:02:34 +0000

    function_pointers: add simple calc using function pointers

---
+++ function_pointers/3-main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - simple calculator
+ * @argc: arg count
+ * @argv: arg vector
+ *
+ * Return: 0 on success, exits with 98/99/100 on errors
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+	int (*f)(int, int);
+	char *op;
+
+	if (argc != 4) /* wrong arg count */
+	{ printf("Error\n"); exit(98); }
+
+	op = argv[2];
+	f = get_op_func(op);
+	if (!f || op[1] != '\0') /* invalid operator or not single char */
+	{ printf("Error\n"); exit(99); }
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	if ((op[0] == '/' || op[0] == '%') && b == 0) /* div by zero */
+	{ printf("Error\n"); exit(100); }
+
+	result = f(a, b);
+	printf("%d\n", result);
+	return (0);
+}
