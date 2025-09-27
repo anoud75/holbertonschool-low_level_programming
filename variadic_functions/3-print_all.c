f5d23581cdbbe8b213f7bba46fabcb26d48845e9
anoud75 <anoudsaud75@gmail.com>
2025-09-27 14:46:23 +0000

    variadic_functions: add Betty-clean 3-print_all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,45 +2,50 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/* helpers keep print_all short for Betty */
+static void p_char(const char **sep, int c)
+{
+	printf("%s%c", *sep, (char)c);
+	*sep = ", ";
+}
+static void p_int(const char **sep, int n)
+{
+	printf("%s%d", *sep, n);
+	*sep = ", ";
+}
+static void p_float(const char **sep, double d)
+{
+	printf("%s%f", *sep, d);
+	*sep = ", ";
+}
+static void p_str(const char **sep, char *s)
+{
+	if (s == NULL)
+		s = "(nil)";
+	printf("%s%s", *sep, s);
+	*sep = ", ";
+}
+
 /**
  * print_all - prints anything according to a format string
- * @format: list of types: 'c' char, 'i' int, 'f' float, 's' char *
- *
- * Unknown specifiers are ignored. Always prints a trailing newline.
+ * @format: types: 'c' char, 'i' int, 'f' float, 's' char *
  */
 void print_all(const char * const format, ...)
 {
 	va_list ap;
 	unsigned int i = 0;
 	const char *sep = "";
-	char *s;
 
 	va_start(ap, format);
-
 	while (format && format[i])
 	{
 		switch (format[i])
 		{
-		case 'c':
-			printf("%s%c", sep, (char)va_arg(ap, int));
-			sep = ", ";
-			break;
-		case 'i':
-			printf("%s%d", sep, va_arg(ap, int));
-			sep = ", ";
-			break;
-		case 'f':
-			printf("%s%f", sep, va_arg(ap, double));
-			sep = ", ";
-			break;
-		case 's':
-			s = va_arg(ap, char *);
-			if (!s) s = "(nil)"; /* only IF in the file */
-			printf("%s%s", sep, s);
-			sep = ", ";
-			break;
-		default:
-			break;
+		case 'c': p_char(&sep, va_arg(ap, int)); break;
+		case 'i': p_int(&sep, va_arg(ap, int)); break;
+		case 'f': p_float(&sep, va_arg(ap, double)); break;
+		case 's': p_str(&sep, va_arg(ap, char *)); break;
+		default: break;
 		}
 		i++;
 	}
